7ee03ffb53ef971021b1e6478902fc00e56ecaf4
esparrs <seve.esparrago@gmail.com>
2013-04-08 02:21:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrouperApp::Application.config.secret_token = 'adb92df30425378d08cde2281b3a4620e4af42e5552594e33fdcfc4c621a9c869ace65a566491bcba9045e17caec962f16d826adac4867bea5a34dcdf7006708'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GrouperApp::Application.config.session_store :cookie_store, key: '_Grouper_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GrouperApp::Application.config.session_store :active_record_store
