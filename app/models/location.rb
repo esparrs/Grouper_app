12fd2490eee0635185a7510b08d27b1dd5958e97
esparrs <seve.esparrago@gmail.com>
2013-04-10 22:25:31 -0400

    Adding Description

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :event_name, :start_time, :end_time, :address, :latitude, :longitude
+  attr_accessible :event_name, :start_time, :end_time, :address, :latitude, :longitude, :description
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
   after_validation :geocode, :reverse_geocode
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408154431) do
+ActiveRecord::Schema.define(:version => 20130411015746) do
 
   create_table "locations", :force => true do |t|
     t.string   "event_name"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(:version => 20130408154431) do
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
   end
 
 end
