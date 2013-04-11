ea1bd5c891f52b659dd149afd2a89d6679e2ac06
esparrs <seve.esparrago@gmail.com>
2013-04-11 01:33:54 -0400

    fourth commit

---
+++ BeautifulSoup_Grouper.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("http://mitcpw.org/schedule/events/2013-04"))
+
+soup = BeautifulSoup("<html>data</html>")
---
+++ page_1_grouper.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("page_1_cpw13.html"))
+
+soup = BeautifulSoup("<html>data</html>")
